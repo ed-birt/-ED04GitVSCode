89541dbf365a7ce05380f7e6664134ecb3e3c6dc
Jaione <jaioperez@birt.eus>
2023-04-20 14:04:42 +0200

    Tercera versión del proyecto

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -2,8 +2,11 @@ public class EjemploGit {
 
     public static void main(String[] args){
 
-        System.out.println("Prueba Git");
+        System.out.println("Prueba Git"); 
 
-    } 
-       
+        System.out.println("Hola mundo!");
+
+        System.out.println("Hola mundo! 2");
+    }    
+
 }
